d65212efd1dd38efa76602a117242bf513fd8d24
Edgar Lee <edgarhinshunlee@gmail.com>
2016-02-19 09:33:36 -0500

    Refactor Camera zenject bindings into own installer

--- Assets/Scripts/Game/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Game/Installers/PlayerInstaller.cs
@@ -13,7 +13,6 @@ namespace MistRidge
         {
             InstallPlayer();
             InstallCollision();
-            InstallCamera();
             InstallSettings();
         }
 
@@ -52,26 +51,8 @@ namespace MistRidge
             Container.Bind<Grounding.Factory>().ToSingle();
         }
 
-        private void InstallCamera()
-        {
-            Container.Bind<CameraOriginView>().ToSinglePrefab(settings.Camera.Prefab);
-            Container.Bind<CameraAnchorView>().ToSinglePrefab(settings.Camera.Prefab);
-            Container.Bind<CameraRigView>().ToSinglePrefab(settings.Camera.Prefab);
-            Container.Bind<CameraView>().ToSinglePrefab(settings.Camera.Prefab);
-            Container.Bind<Camera>().ToSinglePrefab(settings.Camera.Prefab);
-
-            Container.Bind<CameraAnchorManager>().ToSingle();
-            Container.BindAllInterfacesToSingle<CameraAnchorManager>();
-
-            Container.Bind<CameraManager>().ToSingle();
-            Container.BindAllInterfacesToSingle<CameraManager>();
-        }
-
         private void InstallSettings()
         {
-            Container.Bind<CameraAnchorManager.Settings>().ToSingleInstance(settings.Camera.Anchor);
-            Container.Bind<CameraManager.Settings>().ToSingleInstance(settings.Camera.Camera);
-
             Container.Bind<PlayerController.Settings>().ToSingleInstance(settings.Player.Controller);
             Container.Bind<PlayerStateMachine.Settings>().ToSingleInstance(settings.Player.StateMachine);
             Container.Bind<PlayerInternalManager.Settings>().ToSingleInstance(settings.Player.InternalManager);
@@ -88,7 +69,6 @@ namespace MistRidge
         public class Settings
         {
             public PlayerSettings Player;
-            public CameraSettings Camera;
 
             [Serializable]
             public class PlayerSettings
@@ -109,14 +89,6 @@ namespace MistRidge
                     public GameObject DefaultCollidablePrefab;
                     public Grounding.Settings Grounding;
                 }
-            }
-
-            [Serializable]
-            public class CameraSettings
-            {
-                public GameObject Prefab;
-                public CameraAnchorManager.Settings Anchor;
-                public CameraManager.Settings Camera;
             }
         }
     }
