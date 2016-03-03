aaed8aedfe3d0d4f7d6ae7826258ba9bc7865637
Edgar Lee <edgarhinshunlee@gmail.com>
2016-03-03 11:40:33 -0500

    Add moving clouds from object pool

--- Assets/Scripts/Game/Environment/CloudView.cs
+++ Assets/Scripts/Game/Environment/CloudView.cs
@@ -5,12 +5,20 @@ namespace MistRidge
 {
     public class CloudView : PoolInstanceView
     {
+        [SerializeField]
+        private float moveSpeed;
+
         private EllipsoidParticleEmitter cloudEmitter;
 
         public override void OnPoolInstanceReuse()
         {
             cloudEmitter.emit = false;
             cloudEmitter.emit = true;
+        }
+
+        private void Update()
+        {
+            Position += moveSpeed * Up * Time.deltaTime;
         }
 
         private void Awake()
--- Assets/Scripts/Game/Managers/ReadyManager.cs
+++ Assets/Scripts/Game/Managers/ReadyManager.cs
@@ -5,25 +5,30 @@ using Zenject;
 
 namespace MistRidge
 {
-    public class ReadyManager : IInitializable
+    public class ReadyManager : IInitializable, ITickable
     {
         private readonly Settings settings;
         private readonly Spawn spawn;
         private readonly SpawnManager spawnManager;
         private readonly CameraManager cameraManager;
+        private readonly PoolManager poolManager;
         private readonly GameStateSignal.Trigger gameStateTrigger;
+
+        private float timer;
 
         public ReadyManager(
                 Settings settings,
                 Spawn spawn,
                 SpawnManager spawnManager,
                 CameraManager cameraManager,
+                PoolManager poolManager,
                 GameStateSignal.Trigger gameStateTrigger)
         {
             this.settings = settings;
             this.spawn = spawn;
             this.spawnManager = spawnManager;
             this.cameraManager = cameraManager;
+            this.poolManager = poolManager;
             this.gameStateTrigger = gameStateTrigger;
         }
 
@@ -32,11 +37,33 @@ namespace MistRidge
             gameStateTrigger.Fire(GameStateType.Ready);
             spawnManager.CurrentSpawn = spawn;
             cameraManager.CurrentCamera = settings.camera;
+
+            timer = Time.time;
+        }
+
+        public void Tick()
+        {
+            if (Time.time - timer > settings.cloudSpawnSpeed)
+            {
+                timer = Time.time;
+
+                Vector3 position = settings.cloudSpawn.position + (UnityEngine.Random.insideUnitSphere * settings.cloudSpawnRadius);
+
+                poolManager.ReusePoolInstance(
+                    settings.cloudView,
+                    position,
+                    Quaternion.identity
+                );
+            }
         }
 
         [Serializable]
         public class Settings
         {
+            public CloudView cloudView;
+            public Transform cloudSpawn;
+            public float cloudSpawnRadius;
+            public float cloudSpawnSpeed;
             public Camera camera;
         }
     }
