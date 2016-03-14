0a3974cc5f2a445365b6afcc884d954ab33a7fa3
Edgar Lee <edgarhinshunlee@gmail.com>
2016-03-14 09:50:36 -0400

    Readd vertex paint

---
+++ Assets/Scripts/Vendor/VertexPaint/Examples/SplatMapping/Shaders/Editor/SplatMapShaderGUI.cs
@@ -0,0 +1,254 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SplatMapShaderGUI : ShaderGUI 
+{
+
+   void DrawLayer(MaterialEditor editor, int i, MaterialProperty[] props, string[] keyWords, Workflow workflow, bool hasGloss, bool hasSpec, bool isParallax, bool hasEmis)
+   {
+      EditorGUIUtility.labelWidth = 0f;
+      var albedoMap = FindProperty ("_Tex" + i, props);
+      var normalMap = FindProperty ("_Normal" + i, props);
+      var smoothness = FindProperty("_Glossiness" + i, props);
+      var glossinessMap = FindProperty("_GlossinessTex" + i, props, false);
+      var metallic = FindProperty("_Metallic" + i, props, false);
+      var emissionTex = FindProperty("_Emissive" + i, props);
+      var emissionMult = FindProperty("_EmissiveMult" + i, props);
+      var parallax = FindProperty("_Parallax" + i, props);
+      var texScale = FindProperty("_TexScale" + i, props);
+      var specMap = FindProperty("_SpecGlossMap" + i, props, false);
+      var specColor = FindProperty("_SpecColor" + i, props, false);
+
+      editor.TexturePropertySingleLine(new GUIContent("Albedo/Height"), albedoMap);
+      editor.TexturePropertySingleLine(new GUIContent("Normal"), normalMap);
+      if (workflow == Workflow.Metallic)
+      {
+         editor.TexturePropertySingleLine(new GUIContent("Metal(R)/Smoothness(A)"), glossinessMap);
+      }
+      else
+      {
+         editor.TexturePropertySingleLine(new GUIContent("Specular(RGB)/Gloss(A)"), specMap);
+      }
+      if (workflow == Workflow.Metallic && !hasGloss)
+      { 
+         editor.ShaderProperty(smoothness, "Smoothness");
+         editor.ShaderProperty(metallic, "Metallic");
+      }
+      else if (workflow == Workflow.Specular && !hasSpec)
+      {
+         editor.ShaderProperty(smoothness, "Smoothness");
+         editor.ShaderProperty(specColor, "Specular Color");
+      }
+      editor.TexturePropertySingleLine(new GUIContent("Emission"), emissionTex);
+      editor.ShaderProperty(emissionMult, "Emissive Multiplier");
+
+      editor.ShaderProperty(texScale, "Texture Scale");
+
+      if (isParallax)
+      {
+         editor.ShaderProperty(parallax, "Parallax Height");
+      }
+
+      if (i != 1)
+      {
+         editor.ShaderProperty(FindProperty("_Contrast"+i, props), "Interpolation Contrast");
+      }
+   }
+
+   enum Workflow
+   {
+      Metallic = 0,
+      Specular
+   }
+
+   enum FlowChannel
+   {
+      None = 0,
+      One,
+      Two,
+      Three,
+      Four,
+      Five
+   }
+
+   string[] flowChannelNames = new string[]
+   {
+      "None", "One", "Two", "Three", "Four", "Five"
+   };
+
+   public override void OnGUI (MaterialEditor materialEditor, MaterialProperty[] props)
+   {
+      // get the current keywords from the material
+      Material targetMat = materialEditor.target as Material;
+      string[] keyWords = targetMat.shaderKeywords;
+
+      int layerCount = 1;
+      Workflow workflow = Workflow.Metallic;
+      if (targetMat.shader.name == "VertexPainter/SplatBlend_1Layer")
+      {
+         layerCount = 1;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlend_2Layer")
+      {
+         layerCount = 2;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlend_3Layer")
+      {
+         layerCount = 3;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlend_4Layer")
+      {
+         layerCount = 4;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlend_5Layer")
+      {
+         layerCount = 5;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlendSpecular_1Layer")
+      {
+         workflow = Workflow.Specular;
+         layerCount = 1;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlendSpecular_2Layer")
+      {
+         workflow = Workflow.Specular;
+         layerCount = 2;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlendSpecular_3Layer")
+      {
+         workflow = Workflow.Specular;
+         layerCount = 3;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlendSpecular_4Layer")
+      {
+         workflow = Workflow.Specular;
+         layerCount = 4;
+      }
+      else if (targetMat.shader.name == "VertexPainter/SplatBlendSpecular_5Layer")
+      {
+         workflow = Workflow.Specular;
+         layerCount = 5;
+      }
+
+      FlowChannel fchannel = FlowChannel.None;
+      if (keyWords.Contains("_FLOW1"))
+         fchannel = FlowChannel.One;
+      if (keyWords.Contains("_FLOW2"))
+         fchannel = FlowChannel.Two;
+      if (keyWords.Contains("_FLOW3"))
+         fchannel = FlowChannel.Three;
+      if (keyWords.Contains("_FLOW4"))
+         fchannel = FlowChannel.Four;
+      if (keyWords.Contains("_FLOW5"))
+         fchannel = FlowChannel.Five;
+
+      bool flowDrift = keyWords.Contains("_FLOWDRIFT");
+      bool parallax = keyWords.Contains ("_PARALLAXMAP");
+      bool hasGloss = (HasTexture(layerCount, targetMat, "_GlossinessTex"));
+      bool hasSpec = (HasTexture(layerCount, targetMat, "_SpecGlossMap"));
+      bool hasEmis = (HasTexture(layerCount, targetMat, "_Emissive"));
+
+
+      EditorGUI.BeginChangeCheck();
+      Workflow oldWorkflow = workflow;
+      workflow = (Workflow)EditorGUILayout.EnumPopup("Workflow", workflow);
+
+      int oldLayerCount = layerCount;
+      layerCount = EditorGUILayout.IntField("Layer Count", layerCount);
+      if (oldLayerCount != layerCount || workflow != oldWorkflow)
+      {
+         if (layerCount < 1)
+            layerCount = 1;
+         if (layerCount > 5)
+            layerCount = 5;
+
+         if (workflow == Workflow.Metallic)
+         {
+            targetMat.shader = Shader.Find("VertexPainter/SplatBlend_" + layerCount + "Layer");
+         }
+         else
+         {
+            targetMat.shader = Shader.Find("VertexPainter/SplatBlendSpecular_" + layerCount + "Layer");
+         }
+         return;
+      }
+
+
+      parallax = EditorGUILayout.Toggle ("Parallax Offset", parallax);
+
+      for (int i = 0; i < layerCount; ++i)
+      {
+         DrawLayer(materialEditor, i+1, props, keyWords, workflow, hasGloss, hasSpec, parallax, hasEmis);
+
+         EditorGUILayout.Space();
+      }
+
+      EditorGUILayout.Space();
+
+      fchannel = (FlowChannel)EditorGUILayout.Popup((int)fchannel, flowChannelNames);
+      if (fchannel != FlowChannel.None)
+      {
+
+         var flowSpeed = FindProperty("_FlowSpeed", props);
+         var flowIntensity = FindProperty("_FlowIntensity", props);
+
+         materialEditor.ShaderProperty(flowSpeed, "Flow Speed");
+         materialEditor.ShaderProperty(flowIntensity, "Flow Intensity");
+         flowDrift = EditorGUILayout.Toggle("Flow Drift", flowDrift);
+      }
+
+      if (EditorGUI.EndChangeCheck())
+      {
+         var newKeywords = new List<string>();
+
+         newKeywords.Add("_LAYERS" + layerCount.ToString());
+         if (parallax) 
+         {
+            newKeywords.Add("_PARALLAXMAP");
+         }
+         if (HasTexture(layerCount, targetMat, "_Normal"))
+         {
+            newKeywords.Add("_NORMALMAP");
+         }
+         if (hasGloss && workflow == Workflow.Metallic)
+         {
+            newKeywords.Add("_METALLICGLOSSMAP");
+         }
+         if (hasSpec && workflow == Workflow.Specular)
+         {
+            newKeywords.Add("_SPECGLOSSMAP");
+         }
+         if (hasEmis)
+         {
+            newKeywords.Add("_EMISSION"); 
+         }
+         if (fchannel != FlowChannel.None)
+         {
+            newKeywords.Add("_FLOW" + (int)fchannel);
+         }
+
+         if (flowDrift)
+         {
+            newKeywords.Add("_FLOWDRIFT");
+         }
+         targetMat.shaderKeywords = newKeywords.ToArray ();
+         EditorUtility.SetDirty (targetMat);
+      }
+   } 
+
+   bool HasTexture(int numLayers, Material mat, string key)
+   {
+      for (int i = 0; i < numLayers; ++i)
+      {
+         int index = i+1;
+         string prop = key + index;
+         if (mat.HasProperty(prop) && mat.GetTexture(prop) != null)
+            return true;
+      }
+      return false;
+   }
+
+}
---
+++ Assets/Scripts/Vendor/VertexPaint/ReadMe.txt
@@ -0,0 +1,47 @@
+Vertex Painter for Unity
+©2015 Jason Booth
+Tested with Unity 5.3.2p2
+
+Example Video:
+https://www.youtube.com/watch?v=mtbCtwgI440&feature=youtu.be
+
+Donate: https://pledgie.com/campaigns/31113
+
+Additional Vertex Streams
+
+	This package allows you to paint information onto the vertices of a mesh in the Unity editor as well as modify any attribute of the mesh. It uses the new 'additionalVertexStream' system of Unity5, which allows you to override per-instance data on meshes without paying the cost of duplicating a full mesh. This makes it ideal for painting vertex information across many instances of a mesh. The tool also allows you to easily bake that information back to mesh assets if you'd prefer to make a modified mesh and store that in disk, instead of with the instance in the scene. 
+
+Features
+	The toolset contains several different painting modes as well as a few tools that come in handy when doing this type of work. You can modify the positions, normals, UVs and colors of a mesh, painting colors, values, or direction vectors taken from the stroke. You can bake lighting or ambient occlusion data into the vertices, or bake any of the changes you’ve made into a new mesh asset on disk. 
+
+
+Precision
+
+	Different channels of mesh data support different levels of precision. For instance, the color channel stores a color in 8888 format, which means you can only store 0 to 1 values with 256 possible values in each component of the color channel. The 4 UV channels, however, provide 4 32-bit floats each. It is important to understand these limitations when working with vertex data. 
+
+Interface
+
+
+Paint
+	This tool allows you to paint information directly onto the vertices Color or UV channels. Data can be viewed as either color information or greyscale information if you are working on individual channels.
+
+Deform
+	This tool allows you to modify the vertices positions, properly recalculating normals and tangents. 
+
+Flow
+
+	This tool allows you to paint directional vectors into the color or UV channels, which can be used to create flowing effects in shaders. Note that the tool currently computes direction with a dot product of the tangent and bitangent of your mesh- this means that if your tangents are not in the expected space, the flow direction may be computed wrong.
+
+Bake
+	This tab contains a number of utilities, such as the ability to bake AO and lighting into the mesh data. You can also bake information from a texture into your vertices, bake pivot points into the UV channels of the mesh (allowing you to combine many objects and animate them individually in the shader). Finally, you can save meshes as disk assets from this tab.
+
+Shaders
+
+	Included with the package is a ‘SplatBlend’ shader. The SplatBlend shader is closely mirrors Unity’s Standard Shader, but allows you to blend up to 5 different textures together per vertex. The blending is handled via a hight map stored in the alpha channel of the diffuse map, to produce a natural looking blend that allows sand to appear in the cracks of the stone before covering the stone, etc. Values painted into the color channels of the vertices control how much of each texture appears at each vertex. The Specular, Emissive, Metallic, and Normal data is handled in the same manner as with the Standard Shader. 
+
+Each layer has controls for texture scaling, parallax height, and a contrast which controls the width of the blending area between each layer. 
+
+Finally, the shader allows you to designate one layer of the shader to flow mode. This distorts the texture along directional vectors painted into the third UV set (The first UV set is used for your UV mapping, and Unity uses the second UV set for enlighten data). This is useful to create flowing water, lava, or other effects. The flow layer contains controls for speed and intensity, allowing you to modify the effect globally across the material. 
+
+The performance of this shader is highly variable based on how many layers you use and which features you enable. Features which are not used in any layers are compiled out; while a feature used on any layer is computed for every layer. In other words, if you don’t use emissive textures on any layer you won’t pay for the feature, but if you use it on one layer an emissive value will be sampled for every layer.
+
